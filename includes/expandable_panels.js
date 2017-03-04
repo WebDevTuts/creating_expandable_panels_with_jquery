0b6f6ac662378143cd617ed507db0ebcdf33bd9d
edwinchen85 <edwinchen85@gmail.com>
2017-03-05 00:14:31 +0800

    Set initial state of panels to hidden by setting height of the content container equal to 0.

--- includes/expandable_panels.js
+++ includes/expandable_panels.js
@@ -13,7 +13,7 @@ function inject_markup() {
     var link_text = $(this).attr('data-link-text');
     var content = $(this).html();
 
-    $(this).html('<div class="expanding_panel_content_container"><div class="expanding_panel_content">'+content+'</div></div>');
+    $(this).html('<div class="expanding_panel_content_container" style="height:0px;"><div class="expanding_panel_content">'+content+'</div></div>');
 
     $(this).append('<div class="expanding_panel_trigger">'+link_text+'</div>');
 
